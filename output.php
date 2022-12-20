6779d0b799e9ae9899eea44864c0ac1083b0ff4d
Ahmad Al Hariri <53154101+Studyshoot@users.noreply.github.com>
2022-12-20 21:06:02 +0100

    Create output.php

---
+++ output.php
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 
+</head>
+<body>
+    <?php 
+            //  use this shortcode in your WP BlockEditor or as php function Like:
+            //  Block Editor:
+            //  [wp-varquery type="post" status"publish" orderby="rand" per_page="15"]
+            //  or in your theme - Plugin
+            //  echo do_shortcode('[wp-varquery type="post" status"publish" orderby="rand" per_page="15"]')
+
+    ?>
+</body>
+</html>
